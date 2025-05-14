6140e1839fc39284c942a7c7941a09eaa556c42b
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-05-14 12:36:18 +0200

    added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,23 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a City..." />
-        <input className="btn btn-primary" type="submit" value="Search" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a City..."
+              className="w-100 form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              className="btn btn-primary w-100"
+              type="submit"
+              value="Search"
+            />
+          </div>
+        </div>
       </form>
       <h1>New York</h1>
       <ul>
@@ -19,10 +34,11 @@ export default function Weather() {
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
             alt="mostly Cloudy"
           />
-          6°C
+          <span className="Temperature">6</span>{" "}
+          <span className="unit">°C | °F</span>
         </div>
         <div className="col-6">
-          <ul>
+          <ul className="conditions">
             <li>Precipitation: 0%</li>
             <li>Humidity: 36%</li>
             <li>Wind: 8 km/h</li>
