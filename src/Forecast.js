9c80c715071947db69c17d36f3569ef9355d90bc
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-05-21 14:12:02 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -8,17 +8,14 @@ export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
-  function getCity() {
-    let city = props.city;
-    console.log(city);
-    return city;
-  }
+  let city = props.city;
   function DisplayForecast(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
   if (loaded) {
+    console.log(city);
     return (
       <div className="Forecast">
         <div className="row">
@@ -39,7 +36,7 @@ export default function Forecast(props) {
     );
   } else {
     let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${getCity()}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
 
     axios.get(apiUrl).then(DisplayForecast);
     return null;
